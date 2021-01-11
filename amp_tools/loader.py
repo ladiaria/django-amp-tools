aacb7fdb9f67b9d1cae51d860e63e8fd6db2e751
Gustavo Andres Morero <gustavo@netlandish.com>
2021-01-11 16:51:12 -0300

    changes for Django 2.0.13 support.

--- amp_tools/loader.py
+++ amp_tools/loader.py
@@ -16,12 +16,12 @@ class Loader(BaseLoader):
     def get_contents(self, origin):
         return origin.loader.get_contents(origin)
 
-    def get_template_sources(self, template_name, template_dirs=None):
+    def get_template_sources(self, template_name):
         template_name = self.prepare_template_name(template_name)
         for loader in self.template_source_loaders:
             if hasattr(loader, 'get_template_sources'):
                 try:
-                    for result in loader.get_template_sources(template_name, template_dirs):
+                    for result in loader.get_template_sources(template_name):
                         yield result
                 except UnicodeDecodeError:
                     # The template dir name was a bytestring that wasn't valid UTF-8.
@@ -33,28 +33,29 @@ class Loader(BaseLoader):
                     pass
 
     def prepare_template_name(self, template_name):
-        template_name = u'%s/%s' % (get_amp_detect(), template_name)
+        template_name = f'{get_amp_detect()}/{template_name}'
         if settings.AMP_TOOLS_TEMPLATE_PREFIX:
             template_name = settings.AMP_TOOLS_TEMPLATE_PREFIX + template_name
         return template_name
 
-    def load_template(self, template_name, template_dirs=None):
+    def get_template(self, template_name, skip=None):
         template_name = self.prepare_template_name(template_name)
         for loader in self.template_source_loaders:
             try:
-                return loader(template_name, template_dirs)
+                return loader.get_template(template_name, skip)
             except TemplateDoesNotExist:
                 pass
         raise TemplateDoesNotExist("Tried %s" % template_name)
 
-    def load_template_source(self, template_name, template_dirs=None):
+    def get_template_sources(self, template_name):
         template_name = self.prepare_template_name(template_name)
         for loader in self.template_source_loaders:
             if hasattr(loader, 'load_template_source'):
                 try:
-                    return loader.load_template_source(
+                    return loader.get_template_sources(
                         template_name,
-                        template_dirs)
+                        template_dirs,
+                    )
                 except TemplateDoesNotExist:
                     pass
         raise TemplateDoesNotExist("Tried %s" % template_name)
